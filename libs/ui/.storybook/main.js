afaa70d9da1397e99a06e80534cda33468cd3445
Jack Hsu <jack.hsu@gmail.com>
2023-04-19 16:21:53 -0400

    fixed

--- libs/ui/.storybook/main.js
+++ libs/ui/.storybook/main.js
@@ -1,11 +1,13 @@
-module.exports = {
-  core: { builder: 'webpack5' },
-  stories: [
-    '../src/lib/**/*.stories.mdx',
-    '../src/lib/**/*.stories.@(js|jsx|ts|tsx)',
-  ],
+const config = {
+  stories: ['../src/lib/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   addons: ['@storybook/addon-essentials', '@nx/react/plugins/storybook'],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
+  },
 };
+
+export default config;
 
 // To customize your webpack configuration you can use the webpackFinal field.
 // Check https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
